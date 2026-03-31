6b66ea05c4b679aea0d80b634fa7bf8cf8b41251
Adrian Willenbücher <willenbuecher@xq-tec.com>
2026-03-31 06:44:11 +0200

    Skip ComponentDeclaration module layer

--- adapter/src/design.rs
+++ adapter/src/design.rs
@@ -2,15 +2,12 @@
 
 use std::fmt;
 use std::num::NonZeroU32;
-use std::ops::Deref;
 
 use compact_str::CompactString;
 use compact_str::format_compact;
 use ghdl_ast as ast;
-use ghdl_ast::GenericNodeId;
 use hdl_simulation_protocol::design_hierarchy as hierarchy;
 use hdl_simulation_protocol::design_hierarchy::SignalInstanceId;
-use rustc_hash::FxHashMap;
 use serde::Deserialize;
 use tracing::info;
 use tracing::instrument;
@@ -18,18 +15,16 @@ use tracing::instrument;
 #[derive(Debug, Deserialize)]
 pub struct Signal {
     decl: Option<ast::GenericNodeId>,
-    #[serde(skip_deserializing)]
-    name: CompactString,
     #[serde(rename = "type")]
     typ: Type,
 }
 
 #[derive(Debug, Deserialize)]
 struct Instance {
-    stmt: u32,
-    source: u32,
-    #[serde(skip_deserializing)]
-    name: CompactString,
+    #[serde(deserialize_with = "ast::deserialize_optional_node_id")]
+    stmt: Option<ast::GenericNodeId>,
+    #[serde(deserialize_with = "ast::deserialize_optional_node_id")]
+    source: Option<ast::GenericNodeId>,
     objects: Vec<ObjectKind>,
 }
 
@@ -192,9 +187,10 @@ extern "C" fn adapter_register_design(
     let instances = collect_instances(instance_count);
 
     let root_module = build_module(root_instance, &instances, &signals);
+    let root_modules = root_module.into_iter().collect();
     let hierarchy = hierarchy::DesignHierarchy {
         simulation_id: 0,
-        root_modules: vec![root_module],
+        root_modules,
     };
     state.set_design_hierarchy(hierarchy, signals);
     info!("design hierarchy built successfully");
@@ -226,7 +222,7 @@ fn get_signal_name(decl_id: ast::GenericNodeId) -> Option<CompactString> {
         ast::Node::SignalDeclaration(signal) => Some(signal.identifier.normalized.0),
         ast::Node::InterfaceSignalDeclaration(signal) => Some(signal.identifier.normalized.0),
         ast::Node::Attribute(attribute) => {
-            let prefix = retrieve_ast_node(attribute.prefix).ok()?;
+            let prefix = retrieve_ast_node(attribute.prefix.into()).ok()?;
             let prefix: ast::Prefix<'_> = (&prefix).try_into().ok()?;
             let name = match prefix {
                 ast::Prefix::SimpleName(simple_name) => simple_name.identifier.original(),
@@ -238,8 +234,12 @@ fn get_signal_name(decl_id: ast::GenericNodeId) -> Option<CompactString> {
     }
 }
 
-fn retrieve_ast_node(node_id: impl ast::AstNodeId) -> Result<ast::Node, DecodingError> {
-    fn inner(node_id: NonZeroU32) -> Result<ast::Node, DecodingError> {
+fn retrieve_ast_node<T>(node_id: ast::NodeId<T>) -> Result<T, DecodingError>
+where
+    T: TryFrom<ast::Node>,
+    <T as TryFrom<ast::Node>>::Error: fmt::Debug,
+{
+    fn get_node(node_id: NonZeroU32) -> Result<ast::Node, DecodingError> {
         let mut buffer = Vec::with_capacity(4096);
         adapter_encode_ast_node(&mut buffer, node_id);
         serde_json::from_slice::<ast::Node>(&buffer).map_err(|e| DecodingError {
@@ -248,7 +248,7 @@ fn retrieve_ast_node(node_id: impl ast::AstNodeId) -> Result<ast::Node, Decoding
         })
     }
 
-    inner(node_id.to_raw())
+    get_node(node_id.to_raw()).map(|node| node.try_into().expect("node should be of expected type"))
 }
 
 fn collect_signals(signal_count: u32) -> Vec<Signal> {
@@ -256,7 +256,6 @@ fn collect_signals(signal_count: u32) -> Vec<Signal> {
     let mut signals: Vec<Signal> = Vec::with_capacity(1 + signal_count as usize);
     signals.push(Signal {
         decl: None,
-        name: CompactString::new(""),
         typ: Type::Bit {
             left: 0,
             right: 0,
@@ -266,17 +265,7 @@ fn collect_signals(signal_count: u32) -> Vec<Signal> {
 
     for signal_id in 1..=signal_count {
         match retrieve_signal(signal_id) {
-            Ok(mut signal) => {
-                if let Some(node_id) = signal.decl
-                    && let Some(name) = get_signal_name(node_id)
-                {
-                    signal.name = name;
-                } else {
-                    signal.name = CompactString::new("?")
-                }
-                signals.push(signal);
-            },
-
+            Ok(signal) => signals.push(signal),
             Err(e) => {
                 panic!("Error deserializing signal {signal_id}: {e:?}");
             },
@@ -290,9 +279,8 @@ fn collect_instances(instance_count: u32) -> Vec<Instance> {
     // Collect all instances; instance IDs start at 1, so we put a dummy at index 0
     let mut instances: Vec<Instance> = Vec::with_capacity(1 + instance_count as usize);
     instances.push(Instance {
-        stmt: 0,
-        source: 0,
-        name: CompactString::new(""),
+        stmt: None,
+        source: None,
         objects: vec![],
     });
 
@@ -301,22 +289,7 @@ fn collect_instances(instance_count: u32) -> Vec<Instance> {
         buffer.clear();
         adapter_encode_instance(&mut buffer, instance_id);
         match serde_json::from_slice::<Instance>(&buffer) {
-            Ok(mut instance) => {
-                if let Some(node_id) = NonZeroU32::new(instance.stmt) {
-                    buffer.clear();
-                    adapter_encode_ast_node(&mut buffer, node_id);
-                    let node = serde_json::from_slice::<ast::Node>(&buffer).unwrap();
-                    match node {
-                        ast::Node::ComponentInstantiationStatement(stmt) => {
-                            instance.name = stmt.label.original().clone();
-                        },
-                        _ => {
-                            instance.name = CompactString::new("TODO");
-                        },
-                    }
-                }
-                instances.push(instance);
-            },
+            Ok(instance) => instances.push(instance),
             Err(e) => {
                 panic!("Error deserializing instance {instance_id}: {e}");
             },
@@ -330,9 +303,40 @@ fn build_module(
     instance_id: u32,
     instances: &[Instance],
     all_signals: &[Signal],
-) -> hierarchy::Module {
+) -> Option<hierarchy::Module> {
     let instance = &instances[instance_id as usize];
-    let name = instance.name.clone();
+    let name;
+    let kind;
+
+    let stmt = instance
+        .stmt
+        .map(|stmt_id| retrieve_ast_node(stmt_id).unwrap());
+    let source = retrieve_ast_node(instance.source?).ok()?;
+    match source {
+        ast::Node::ArchitectureBody(arch) => {
+            let entity_name = retrieve_ast_node(arch.entity_name).unwrap();
+            kind = hierarchy::ModuleKind::DesignEntity {
+                entity: entity_name.identifier.into_original(),
+                architecture: arch.identifier.into_original(),
+            };
+
+            match stmt {
+                Some(ast::Node::ComponentInstantiationStatement(stmt)) => {
+                    name = Some(stmt.label.into_original());
+                },
+                _ => name = None,
+            }
+        },
+        ast::Node::ComponentDeclaration(_) => {
+            if let &[ObjectKind::Instance { id: inner }] = &*instance.objects {
+                return build_module(inner, instances, all_signals);
+            } else {
+                return None;
+            }
+        },
+        _ => return None,
+    }
+
     let mut submodules = vec![];
     let mut signals = vec![];
 
@@ -342,8 +346,15 @@ fn build_module(
                 val_kind: ValKind::Signal { id: signal_id },
             } => {
                 let signal = &all_signals[signal_id.get() as usize];
+                let name = if let Some(node_id) = signal.decl
+                    && let Some(name) = get_signal_name(node_id)
+                {
+                    name
+                } else {
+                    CompactString::const_new("?")
+                };
                 signals.push(hierarchy::Signal {
-                    name: signal.name.clone(),
+                    name,
                     id: SignalInstanceId(*signal_id),
                     typ: (&signal.typ).into(),
                 });
@@ -352,21 +363,19 @@ fn build_module(
                 // TODO
             },
             ObjectKind::Instance { id } => {
-                let child_module = build_module(*id, instances, all_signals);
-                submodules.push(child_module);
+                if let Some(child_module) = build_module(*id, instances, all_signals) {
+                    submodules.push(child_module);
+                }
             },
         }
     }
 
-    hierarchy::Module {
+    Some(hierarchy::Module {
         name,
-        kind: hierarchy::ModuleKind::DesignEntity {
-            entity: CompactString::new("TODO"),
-            architecture: CompactString::new("TODO"),
-        },
+        kind,
         submodules,
         signals,
-    }
+    })
 }
 
 unsafe extern "C" {
