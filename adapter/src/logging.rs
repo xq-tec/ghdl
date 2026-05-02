0c0072213edc2a4e670676d4b9f3d837e248824a
Adrian Willenbücher <willenbuecher@xq-tec.com>
2026-05-02 13:36:02 +0200

    Use GHDL_LOG_OTLP to choose between stderr and OpenTelemetry

--- adapter/src/logging.rs
+++ adapter/src/logging.rs
@@ -1,7 +1,6 @@
 use std::io;
 
 use opentelemetry::KeyValue;
-use opentelemetry::global;
 use opentelemetry::trace::TracerProvider as _;
 use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
 use opentelemetry_sdk::Resource;
@@ -12,11 +11,44 @@ use tracing_opentelemetry::OpenTelemetryLayer;
 use tracing_subscriber::layer::SubscriberExt as _;
 use tracing_subscriber::util::SubscriberInitExt as _;
 
-fn resource() -> Resource {
-    Resource::builder()
-        .with_service_name("ghdl-adapter")
-        .with_attributes([KeyValue::new("service.version", env!("CARGO_PKG_VERSION"))])
-        .build()
+/// Initializes logging for the adapter.
+///
+/// The `GHDL_LOG_LEVEL` environment variable is used to set the logging level and filters.
+/// The default log destination is *stderr*;
+/// this can be overridden by setting the `GHDL_LOG_OTLP` environment variable to select OpenTelemetry.
+pub(crate) fn init_logging() {
+    let mut log_target = tracing_subscriber::EnvFilter::builder()
+        .with_default_directive(LevelFilter::WARN.into())
+        .with_env_var("GHDL_LOG_LEVEL")
+        .from_env_lossy();
+
+    if let Ok(otlp_config) = std::env::var("GHDL_LOG_OTLP") {
+        if let Ok(directive) = otlp_config.parse::<tracing_subscriber::filter::Directive>() {
+            log_target = log_target.add_directive(directive);
+        }
+
+        let tracer_provider = init_tracer_provider();
+        let logger_provider = init_logger_provider();
+
+        opentelemetry::global::set_tracer_provider(tracer_provider.clone());
+
+        let tracer = tracer_provider.tracer("ghdl-adapter");
+        let otel_trace_layer = OpenTelemetryLayer::new(tracer);
+        let otel_log_layer = OpenTelemetryTracingBridge::new(&logger_provider);
+        tracing_subscriber::registry()
+            .with(log_target)
+            .with(otel_trace_layer)
+            .with(otel_log_layer)
+            .init();
+    } else {
+        let stderr_layer = tracing_subscriber::fmt::layer()
+            .with_target(true)
+            .with_writer(io::stderr);
+        tracing_subscriber::registry()
+            .with(log_target)
+            .with(stderr_layer)
+            .init();
+    }
 }
 
 fn init_tracer_provider() -> SdkTracerProvider {
@@ -43,35 +75,9 @@ fn init_logger_provider() -> SdkLoggerProvider {
         .build()
 }
 
-/// Initializes the OpenTelemetry tracing subscriber.
-///
-/// Sets up a composite subscriber with:
-/// - An `fmt` layer for stderr output (local debugging)
-/// - An `EnvFilter` for level filtering (defaults to `info`)
-/// - An OpenTelemetry trace layer (spans via OTLP/gRPC)
-/// - An OpenTelemetry log layer (events via OTLP/gRPC)
-pub(crate) fn init_logging() {
-    let tracer_provider = init_tracer_provider();
-    let logger_provider = init_logger_provider();
-
-    global::set_tracer_provider(tracer_provider.clone());
-
-    let tracer = tracer_provider.tracer("ghdl-adapter");
-    let otel_trace_layer = OpenTelemetryLayer::new(tracer);
-    let otel_log_layer = OpenTelemetryTracingBridge::new(&logger_provider);
-
-    let log_target = tracing_subscriber::EnvFilter::builder()
-        .with_default_directive(LevelFilter::WARN.into())
-        .with_env_var("GHDL_LOG_LEVEL")
-        .from_env_lossy();
-    tracing_subscriber::registry()
-        .with(log_target)
-        .with(
-            tracing_subscriber::fmt::layer()
-                .with_target(true)
-                .with_writer(io::stderr),
-        )
-        .with(otel_trace_layer)
-        .with(otel_log_layer)
-        .init();
+fn resource() -> Resource {
+    Resource::builder()
+        .with_service_name("ghdl-adapter")
+        .with_attributes([KeyValue::new("service.version", env!("CARGO_PKG_VERSION"))])
+        .build()
 }
